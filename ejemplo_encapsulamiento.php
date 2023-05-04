860cb9bb9eabf30de5e07900813df1896ba1d36c
Rybensx <anderson_arf@hotmail.es>
2023-05-04 13:27:01 -0600

    hola mundo

--- ejemplo_encapsulamiento.php
+++ ejemplo_encapsulamiento.php
@@ -11,23 +11,23 @@ class producto {
     public function __construct(string $nombre, int $precio, bool $disponible)
     {
         # SE LES DA VALORES A LOS ATRIBUTOS CON LOS PARAMETROS QUE RECIBE EL METODO CONSTRUCTOR
-        $this ->nombre = $nombre;
-        $this ->precio = $precio;
-        $this ->disponible = $disponible;
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponible = $disponible;
     }
 
     #DEFINICION DE METODOS
     public function mostrarProducto() {
-        echo "El Producto es; " . $this -> nombre . " y su precio es de: " . $this ->precio;
+        echo "El Producto es; " . $this->nombre . " y su precio es de: " . $this->precio;
     }
     #METODO GETTER PARA LA PROPIEDAD PROTEGIDA DE NOMBRE
     public function getNombre(): string {
-        return $this-> nombre;
+        return $this->nombre;
     }
     #METODO SETTER PARA PROPIEDAD PROTEGIDA DE NOMBRE
 
-public function setNombre (string $nombre) {
-    $this -> nombre = $nombre;
+    public function setNombre (string $nombre) {
+    $this->nombre = $nombre;
     }
 
 }
@@ -36,7 +36,7 @@ $producto = new Producto ('Tablet', 200, true);
 // $producto->mostrarProducto();
 // echo $producto->nombre //PRODUCE UN ERROR
     echo $producto->getNombre(); //OBTIENE EL NOMBRE
-    $producto->setNombre('nuevo Nombre'); //SETEA EL NOMBRE
+    $producto->setNombre('Nuevo Nombre'); //SETEA EL NOMBRE
     echo $producto->getNombre(); // OBTIENE EL NOMBRE
 
 ?>
